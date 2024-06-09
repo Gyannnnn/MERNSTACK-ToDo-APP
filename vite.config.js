5f8764bbcc112ee72cbaac5dee6b46ab70dfa963
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-09 08:48:16 +0530

    Uppdated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 });
