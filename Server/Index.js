fca535b55fd87ffa80287e7b2643f197407a7c4b
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-09 16:20:20 +0530

    Update Index.js

--- Server/Index.js
+++ Server/Index.js
@@ -4,19 +4,30 @@ const cors = require('cors');
 const todoModel = require('./Models/Todo');
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+    
 .then(() => {
     console.log("Connected to MongoDB");
 })
 .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
 });
+
+app.get("/", (req, res) => {
+    res.json("Working fine");
+})
 
 app.get('/get', (req, res) => {
     todoModel.find()
